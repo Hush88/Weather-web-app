88b150ec451143d8e90524d3adead4add0ffe225
Dima Dediukhin <dde6156@gmail.com>
2024-07-17 09:43:43 +0300

    initial commit

---
+++ js/api.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const api_key = "30e072648fc75ba8d1301d192c7bcccf";
+
+/**
+ * Fetch data from server
+ * @param {string} URL  Api URL
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function (URL, callback) { 
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) { 
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query : "London" , "New York"
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
